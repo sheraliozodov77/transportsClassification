5a4140c463cfeb8dc4d365a24a952201b86f13cf
Sherali Ozodov <sheraliozodov2003@gmail.com>
2022-06-11 10:29:43 +0500

    upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from fastai.vision.all import *
+import streamlit as st
+import platform
+import pathlib
+import plotly.express as px
+# plat = platform.system()
+# if plat == 'Linux': pathlib.PosixPath = pathlib.WindowsPath
+
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+## title
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+# rasmni joylash
+file = st.file_uploader('Rasm yuklash', type=['png','jpeg','gif','svg','pkl'])
+if file:
+    st.image(file)
+    # PIL konvert
+    img = PILImage.create(file)
+    # model
+    model = load_learner('transport_model.pkl')
+
+    #predict
+    pred, prod_id , probs  = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Probability: {probs[prod_id]*100:0.1f}%")
+
+    #plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
+
